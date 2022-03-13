96c7d61d67555615dbabe0e1d592c10e24371884
Rhesa Daiva Bremana <rhesa.daiva@gmail.com>
2022-03-13 09:41:46 +0700

    add button generate message without whatsapp link

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -20,9 +20,9 @@ const UploadForm = () => {
             const fixNumber = "62" + formattedNumber;
 
             // const urlEncoded = encodeURIComponent(pureUrl)
-            const urlEncoded = encodeURIComponent(pureUrl);
+            const encodedUrl = encodeURIComponent(pureUrl);
 
-            let message = `Salam%20Sejahtera%20untuk%20kita%20semua..%0ASelamat%20Malam%20Komandan%2C%20Senior%20dan%20Rekan-rekan%20terhormat%0A%0AKami%20yang%20berbahagia%2C%20mengundang%20Saudara%2FSaudari%20untuk%20hadir%20di%20acara%20pernikahan%20kami%20melalui%20undangan%20online%20yang%20kami%20persembahkan%20sebagai%20berikut%20%3A%0A%0A${urlEncoded}%0A%0ARegards%2C%0ARhesa%20Daiva%20Bremana%20Ginting%0AOriettha%20Deany%20br.%20Lumbantoruan%0A%0A%0A`;
+            let message = `Shalom%2C%20salam%20sejahtera%20untuk%20kita%20semua.%20%0A%0ATanpa%20mengurangi%20rasa%20hormat%2C%20melalui%20media%20ini%2C%20izinkan%20kami%20mengundang%20Bapak%2FIbu%2FSaudara%2Fi%2Fteman-teman%20sekalian%2C%20untuk%20berkenan%20hadir%20dalam%20acara%20Pemberkatan%20Pernikahan%20%26%20Adat%2FResepsi%20kami%3B%0A%0Arincian%20acara%3A%0A${encodedUrl}%0A%0AKami%20memohon%20maaf%20karena%20keterbatasan%20jarak%20dan%20waktu%2C%20tidak%20dapat%20mengirimkan%20undangan%20ini%20secara%20langsung.%20%0A%0AAtas%20kehadiran%20dan%20doa%20restunya%2C%20kami%20beserta%20keluarga%20menyampaikan%20terimakasih.%20%0AKiranya%20Tuhan%20memberkati%20kita%20semua.%20%0A%0AKami%20yang%20berbahagia%2C%20%0ARhesa%20%26%20Oriettha`;
             console.log(message, 'message');
 
             let url = `https://api.whatsapp.com/send?phone=${fixNumber}&text=${message}`;
@@ -30,6 +30,11 @@ const UploadForm = () => {
 
         }
     };
+
+
+    const generateMessageOnly = (url) => {
+        return `Shalom, salam sejahtera untuk kita semua.\n\nTanpa mengurangi rasa hormat, melalui media ini, izinkan kami mengundang Bapak/Ibu/Saudara/i/teman-teman sekalian, untuk berkenan hadir dalam acara Pemberkatan Pernikahan & Adat/Resepsi kami;\n\nrincian acara:\n${url}\n\nKami memohon maaf karena keterbatasan jarak dan waktu, tidak dapat mengirimkan undangan ini secara langsung.\n\nAtas kehadiran dan doa restunya, kami beserta keluarga menyampaikan terimakasih. \nKiranya Tuhan memberkati kita semua. \n\nKami yang berbahagia,\nRhesa & Oriettha`
+    }
 
     const handleResetData = () => {
         setDataTable([]);
@@ -73,11 +78,9 @@ const UploadForm = () => {
                     {dataTable.map((items, key) => {
 
 
-                       let url = `https://rhesa-oriettha.vercel.app/?to=${encodeURIComponent(items.nama)}`;
-                       let pureUrl = `https://rhesa-oriettha.vercel.app/?to=${items.nama}`;
+                    //    let url = `https://rhesa-oriettha.vercel.app/?to=${encodeURIComponent(items.nama)}`;
+                       let url = `https://rhesa-oriettha.vercel.app/?to=${items.nama.replace(/\s/g,"+")}`;
 
-                       let waUrl = `https://api.whatsapp.com/send?phone=6281234567890&text=Saya%20tertarik%20untuk%20membeli%20produk%20ini%20segera.`;
-                       
                        return (    
                         <tr key={key + 1}>
                             <th scope="row" className="text-center">{key + 1}</th>
@@ -85,8 +88,9 @@ const UploadForm = () => {
                             <td>{items.no_hp}</td>
                             <td>{url}</td>
                             <td className="text-center">
-                                <Button size="sm" style={{marginRight:"20px"}} color="success" onClick={() => {navigator.clipboard.writeText(url)}}>Url Undangan</Button>
-                                <Button size="sm" color="primary" onClick={() => {window.open(whatsappUrlCreator(items.no_hp, url),'_blank')}}>Url Whatsapp</Button>
+                                <Button size="sm" style={{marginRight:"10px"}} color="success" onClick={() => {navigator.clipboard.writeText(url)}}>Url Undangan</Button>
+                                <Button size="sm" style={{marginRight:"10px"}} color="primary" onClick={() => {window.open(whatsappUrlCreator(items.no_hp, url),'_blank')}}>Url Whatsapp</Button>
+                                <Button size="sm" color="info" onClick={() => {navigator.clipboard.writeText(generateMessageOnly(url))}}>Pesan Whatsapp</Button>
                             </td>
                             {/* <td>{url}</td> */}
                         </tr>
